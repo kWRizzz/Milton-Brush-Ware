4dbe98c258c976ff59d8dd253eceb15528ee96dd
kWRizzz <krishnathey2j@gmail.com>
2026-05-12 22:58:54 +0530

    feature section made

---
+++ frontend/src/sections/home/Features.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+    ShieldCheck,
+    Settings,
+    Factory,
+    Globe,
+} from "lucide-react";
+
+
+const features = [
+    {
+        icon: ShieldCheck,
+        title: "Premium content",
+        description: "Manufactured using high-quality materials and precision engineering standards."
+    },
+    {
+        icon: Settings,
+        title: "Custom Solutions",
+        description: "Customized industrial brushes designed according to your business needs. "
+    },
+    {
+        icon: Factory,
+        title: "Industrial Manufacturing",
+        description:
+            "Advanced production process ensuring durability and reliable performance.",
+    },
+    {
+        icon: Globe,
+        title: "Global Supply",
+        description:
+            "Serving industrial clients with efficient supply and trusted partnerships.",
+    }
+]
+
+
+
+
+const Features = () => {
+    return (
+        <section
+            className=' section-padding bg-white'
+        >
+
+
+        </section>
+    )
+}
+
+export default Features
