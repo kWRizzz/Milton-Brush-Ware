f4ab647e8f44d41f6c0086b11faf5e88b812b20e
kWRizzz <krishnathey2j@gmail.com>
2026-05-12 23:27:03 +0530

    feature section made

--- frontend/src/app/(website)/page.js
+++ frontend/src/app/(website)/page.js
@@ -1,3 +1,4 @@
+import Features from "@/src/sections/home/Features";
 import Hero from "@/src/sections/home/Hero";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
       <>
         <Hero/>
+        <Features/>
       </>
   );
 }
--- frontend/src/sections/home/Features.jsx
+++ frontend/src/sections/home/Features.jsx
@@ -41,6 +41,66 @@ const Features = () => {
             className=' section-padding bg-white'
         >
 
+            <div
+                className='container-custom '
+            >
+
+                {/* heading */}
+                <div
+                    className=''
+                >
+                    <p className="text-primary uppercase tracking-widest text-sm font-semibold mb-3">
+                        Why Choose Us
+                    </p>
+
+                    <h2 className="heading-secondary mb-4">
+                        Trusted Industrial Brush Manufacturers
+                    </h2>
+
+                    <p className="paragraph max-w-2xl mx-auto">
+                        We deliver precision-engineered brush solutions designed
+                        for industrial durability, efficiency, and long-term performance.
+                    </p>
+                </div>
+
+                <div
+                    className='  flex justify-center items-center gap-5'
+                >
+                    {
+                        features.map((feature,index)=>{
+                            const Icon= feature.icon
+                            return (
+
+                                <div
+                                    key={index}
+                                    className=' flex flex-col  justify-center items-center  border border-border rounded-2xl px-8  hover:shadow-lg transition bg-background'
+                                >
+
+                                    <div
+                                        className="w-14 h-14 rounded-xl bg-primary text-white flex items-center justify-center mb-6"
+                                    >
+                                        <Icon
+                                            size={28}
+                                        />
+                                    </div>
+
+                                    <div
+                                        className=' w-full text-xl font-bold mb-4'
+                                    >
+                                        {feature.title}
+                                    </div>
+
+                                    <div
+                                        className='paragraph'
+                                    >
+                                        {feature.description}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
 
         </section>
     )
